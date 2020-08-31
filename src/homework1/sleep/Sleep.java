7b43d13153bf9e5cfa704a7229fd507ce260ef35
Dmitry23333 <68802269+Dmitry23333@users.noreply.github.com>
2020-08-31 22:10:18 +0300

    update

--- src/homework1/sleep/Sleep.java
+++ src/homework1/sleep/Sleep.java
@@ -3,17 +3,14 @@ package homework1.sleep;
 public class Sleep {
     public static void main (String[] arg){
         boolean weekday=false;
-        boolean vacation=true;
+        boolean vacation=false;
        if (SleepIn(weekday,vacation)){
            System.out.println("Можем спать дальше");
        }
        else System.out.println("Пора идти на работу");
     }
     public static boolean SleepIn (boolean weekday, boolean vacation) {
-        if (weekday) {
-            return false;
-        }
-        if (vacation) {
+        if (!weekday||vacation) {
             return true;
         }
         return false;
