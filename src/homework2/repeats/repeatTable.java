fbe942f9860811d1619a40578b09f3cafbf2e572
Dmitry23333 <68802269+Dmitry23333@users.noreply.github.com>
2020-09-06 22:44:53 +0300

    add

--- src/homework2/repeats/repeatTable.java
+++ src/homework2/repeats/repeatTable.java
@@ -2,19 +2,19 @@ package homework2.repeats;
 
 public class repeatHome5 {
     public static void main(String[] args) {
-        for (int i =1; i <=10; i++) {
-            if(i==5){
-                System.out.print("\r");
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 2; j <= 5; j++) {
+                System.out.print(+j + "x" + i + "=" + (j * i) + "\t");
             }
-            for (int j = 2; j <= 9; j++) {
-                if(i<10){
-                    System.out.print(j + "x" + i + "= " + (j * i)+"\t");
-                }else {
-                   System.out.print(j + "x" + i + "=" + (j * i)+"\t");
-                }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 6; j <= 9; j++) {
+                System.out.print(+j + "x" + i + "=" + (j * i) + "\t");
+
             }
-             System.out.println();
+            System.out.println();
         }
     }
-
 }
