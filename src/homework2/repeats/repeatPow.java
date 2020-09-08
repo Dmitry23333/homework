dd825a2d2000ba699b77c5264f9eabd94ec59f22
Dmitry23333 <68802269+Dmitry23333@users.noreply.github.com>
2020-09-08 11:11:46 +0300

    check add power

--- src/homework2/repeats/repeatPow.java
+++ src/homework2/repeats/repeatPow.java
@@ -6,25 +6,21 @@ public class repeatPow {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число ");
-        double number1 =0;
         while(!in.hasNextDouble ()){
             System.out.println("Повторить ввод. Введите число");
-            in.next();
+            in.nextLine();
         }
-        number1 =  in.nextDouble();
+        Double number1 =  in.nextDouble();
         Scanner in1 = new Scanner(System.in);
+        String regex="\\d+";
         System.out.print("Введите степень  ");
-        int number2 = 0;
-        while(!in1.hasNextInt()){
-            System.out.println("Повторить ввод. Степень должна быть равна  целому число");
-            in1.next();
+        String number2=in1.nextLine();
+        while(!number2.matches(regex)){
+            System.out.println("Повторить ввод. Степень равна положительному целому числу");
+            number2=in1.nextLine();
         }
-        number2=in1.nextInt();
-        while (number2<0){
-           System.out.println("Введите степень больше или равную 0");
-           number2=in1.nextInt();
-        }
-           System.out.println(number1 +" в степени "+number2 + "= "+Math.pow(number1,number2));
+        int power=Integer.parseInt(number2);
+        System.out.println(number1 +" в степени "+number2 + "= "+Math.pow(number1,power));
     }
 }
 
