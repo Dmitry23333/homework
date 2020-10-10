6dca12d5950168fff4c40c926645d6883d73d827
Dmitry23333 <68802269+Dmitry23333@users.noreply.github.com>
2020-10-10 17:19:04 +0300

    update home1

--- src/homework1/phonenumber/PhoneNumber.java
+++ src/homework1/phonenumber/PhoneNumber.java
@@ -3,9 +3,9 @@ package homework1.phonenumber;
 public class PhoneNumber {
     public static void main (String [] arg){
         int[] number= new int[] {0,1,2,3,4,5,6,7,8,9};
-        System.out.print(CreatePhoneNumber(number));
+        System.out.print(createPhoneNumber(number));
     }
-    public static String CreatePhoneNumber(int [] phone){
+    public static String createPhoneNumber(int [] phone){
         char[] numberphone= new char[]{'(','0','0','0',')',' ','0','0','0','-','0','0','0','0'};
         int j=0;
         for (int i=0;i<numberphone.length;i++){
