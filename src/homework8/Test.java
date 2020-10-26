c0ce483282e735409f4b7b39a08e9cf1633dcbc6
Dmitry23333 <68802269+Dmitry23333@users.noreply.github.com>
2020-10-26 09:30:29 +0300

    Update Test.java

--- src/homework8/Test.java
+++ src/homework8/Test.java
@@ -2,8 +2,9 @@ package homework8;
 
 import java.io.*;
 
-import static home8.SiteLoader.Currency.EUR;
-import static home8.SiteLoader.Currency.USD;
+import static homework8.SiteLoader.Currency.EUR;
+import static homework8.SiteLoader.Currency.USD;
+import static homework8.SiteLoader.Currency.RUB;
 
 public class Test {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class Test {
     public static void printRates(SiteLoader loader) {
         System.out.println(loader.load(USD));
         System.out.println(loader.load(EUR));
-        System.out.println(loader.load(USD));
+        System.out.println(loader.load(RUB));
     }
     public static void saveUSDRates(SiteLoader loader){
         loader.saveRate(loader.load(SiteLoader.Currency.USD),SiteLoader.Currency.USD);
