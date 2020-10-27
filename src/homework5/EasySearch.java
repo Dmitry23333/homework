2d22f713ab243dfcfd3e86ee731deb006f9ad8b9
Dmitry23333 <68802269+Dmitry23333@users.noreply.github.com>
2020-10-27 21:42:14 +0300

    Update EasySearch.java

--- src/homework5/EasySearch.java
+++ src/homework5/EasySearch.java
@@ -4,11 +4,12 @@ public class EasySearch implements ISearchEngine {
 
     public long search(String text, String word) {
         text=text.toLowerCase();
+        word=" "+word.toLowerCase()+" ";
         int count=0;
         int index = 0;
         int wordLength=0;
         while(index != -1){
-            index = text.indexOf(" "+word.toLowerCase()+" ", index + wordLength);
+            index = text.indexOf(word, index + wordLength);
             if (index != -1) {
                 count++;
             }
